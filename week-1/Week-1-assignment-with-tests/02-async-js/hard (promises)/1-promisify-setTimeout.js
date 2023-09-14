713f2323551ec61181092e5125000da297a83627
Pallabch <pallabchakraborty222@@gmail.com>
2023-09-14 10:24:07 +0530

    Worked on assignments 1 and 2

--- week-1/Week-1-assignment-with-tests/02-async-js/hard (promises)/1-promisify-setTimeout.js
+++ week-1/Week-1-assignment-with-tests/02-async-js/hard (promises)/1-promisify-setTimeout.js
@@ -3,4 +3,22 @@
 */
 
 function wait(n) {
+    return new Promise((resolve,reject)=>{
+        if(n<0){
+            reject(new Error('Time must not be negative'));
+        }else {
+            setTimeout(() =>{
+                resolve();
+            }, n * 1000);
+        }
+    });
 }
+
+var seconds = 3;
+wait(seconds)
+    .then(() => {
+        console.log("Function Resolved after " + seconds + " seconds.");
+})
+    .catch((error) => {
+        console.error(error);
+    });
